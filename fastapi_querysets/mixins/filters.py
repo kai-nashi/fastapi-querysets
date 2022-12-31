44b09efd987235ca38971b81579a697ea6de9ed9
Nikakto <black-z@bk.ru>
2022-12-31 13:39:25 +0300

    feat: FilterMixin created

---
+++ fastapi_querysets/mixins/filters.py
@@ -0,0 +1,58 @@
+import copy
+import dataclasses
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Protocol
+from typing import Tuple
+
+from fastapi_depends_ext import DependsMethod
+from pydantic.fields import FieldInfo
+from starlette.requests import Request
+from tortoise.queryset import QuerySet
+
+
+class DataclassProtocol(Protocol):
+    __dataclass_fields__: Dict
+    __dataclass_params__: Dict
+    __post_init__: Optional[Callable]
+
+
+class FilterMixin:
+    filter_class: DataclassProtocol
+
+    @property
+    def exclude_class(self) -> type:
+        fields: List[Tuple[str, type, dataclasses.Field]] = []
+        for field in dataclasses.fields(self.filter_class):
+            _field = copy.copy(field)
+            if isinstance(_field.default, FieldInfo):
+                _field.default = copy.copy(_field.default)
+                _field.default.alias = f"{_field.default.alias or _field.name}!"
+
+            fields.append((_field.name, _field.type, _field))
+
+        return dataclasses.make_dataclass(f"{self.filter_class.__name__}Negation", fields=fields, frozen=True)
+
+    def _get_model_filters(self, request: Request, filters: DataclassProtocol) -> Dict[str, Any]:
+        fields_map = {field.default.alias or field.name: field.name for field in dataclasses.fields(filters)}
+        _fields = set(request.query_params) & set(fields_map)
+        return {fields_map[field]: getattr(filters, fields_map[field]) for field in _fields}
+
+    def get_request_queryset(
+        self,
+        request: Request,
+        exclude: DataclassProtocol = DependsMethod("exclude_class"),
+        filters: DataclassProtocol = DependsMethod("filter_class"),
+        queryset: QuerySet = DependsMethod("get_request_queryset", from_super=True),
+    ) -> QuerySet:
+
+        if model_filters := self._get_model_filters(request, filters):
+            queryset = queryset.filter(**model_filters)
+
+        if model_exclude := self._get_model_filters(request, exclude):
+            queryset = queryset.exclude(**model_exclude)
+
+        return queryset
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,15 +1,15 @@
 import asyncio
 
 import pytest
+from tortoise.expressions import F
 
 from tests.app_models.tortoise_orm import Contract
 from tests.app_models.tortoise_orm import Worker
 
 
 @pytest.fixture(scope="function")
-async def db_create_workers():
-    await Worker.all().delete()
-    await Worker.bulk_create(objects=[Worker(id=index + 1, name=f"Test Worker {index}") for index in range(100)])
+async def db_clean():
+    await asyncio.gather(Worker.all().delete())
 
 
 @pytest.fixture(scope="function")
@@ -19,5 +19,11 @@ async def db_create_contracts():
 
 
 @pytest.fixture(scope="function")
-async def db_clean():
-    await asyncio.gather(Worker.all().delete())
+async def db_create_workers():
+    await Worker.all().delete()
+    await Worker.bulk_create(objects=[Worker(id=index + 1, name=f"Test Worker {index}") for index in range(100)])
+
+
+@pytest.fixture(scope="function")
+async def db_fill(db_clean, db_create_contracts, db_create_workers):
+    await Worker.all().update(contract_id=F("id"))
