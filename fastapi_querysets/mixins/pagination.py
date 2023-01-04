8b4ea05b10a8da6cbe6e3c25e36a76477078013f
Nikakto <black-z@bk.ru>
2023-01-04 17:32:22 +0300

    chore: PaginationMixin remove tortoise-orm error description

--- fastapi_querysets/mixins/pagination.py
+++ fastapi_querysets/mixins/pagination.py
@@ -42,47 +42,6 @@ class PaginationMixin:
         pagination: Pagination = DependsMethod("_pagination"),
     ) -> QuerySet:
         total = await queryset.count()
-        # on "missing FROM-clause entry for table" (https://github.com/tortoise/tortoise-orm/issues/973)
-        # if solved -> update tortoise-orm
-        # else:
-        # for local develop -> improves site package code
-        # for production -> fork tortoise-orm and improve in forked repo
-        #
-        # /tortoise/query_utils.py
-        # ...
-        #
-        # class Q:
-        #     ...
-        #
-        #     def _resolve_custom_kwarg(
-        #             self, model: "Type[Model]", key: str, value: Any, table: Table
-        #     ) -> QueryModifier:
-        #         having_info = self._custom_filters[key]
-        #         annotation = self._annotations[having_info["field"]]
-        #         if isinstance(annotation, Term):
-        #             annotation_info = {"field": annotation}
-        #         else:
-        #             annotation_info = annotation.resolve(model, table)
-        #
-        #         joins = []
-        #         for (table, related_field_name, related_field) in annotation_info.get('joins', []):
-        #             joins.extend(_get_joins_for_related_field(table, related_field, related_field_name))
-        #
-        #         operator = having_info["operator"]
-        #         overridden_operator = model._meta.db.executor_class.get_overridden_filter_func(
-        #             filter_func=operator
-        #         )
-        #         if overridden_operator:
-        #             operator = overridden_operator
-        #         if annotation_info["field"].is_aggregate:
-        #             modifier = QueryModifier(having_criterion=operator(annotation_info["field"], value),
-        #                                      joins=joins)
-        #         else:
-        #             modifier = QueryModifier(where_criterion=operator(annotation_info["field"], value), joins=joins)
-        #         return modifier
-        #
-        # ...
-
         response.headers["x-page"] = str(math.ceil(pagination.skip / pagination.limit) + 1)
         response.headers["x-pages"] = str(math.ceil(total / pagination.limit))
         response.headers["x-per-page"] = str(pagination.limit)
